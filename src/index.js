61b122633ae5d088b7784d39aaa06e12056d3617
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-10-18 05:39:07 +0100

    Update main.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./style.css";
 import {getDefault} from "./functions/renderDefault";
 import {getHome} from "./functions/renderHome";
+import { clearContents } from "./functions/clear";
 
 (function deploy(){
   getDefault();
   getHome();
+  const homeLink = document.querySelector("#home");
+  const menuLink = document.querySelector("#menu");
+  const contactLink = document.querySelector("#contact")
+  homeLink.addEventListener("click", ()=>{
+    clearContents();
+    getHome();
+  });
+  menuLink.addEventListener("click", ()=>{
+    clearContents();
+  });
+  contactLink.addEventListener("click", ()=>{
+    clearContents();
+  });
   
 })();
 
