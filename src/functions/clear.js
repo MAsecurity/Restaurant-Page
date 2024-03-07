a7ad34997bf809014bf3ddae27d1ecf65e7bea8a
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-03-07 16:39:10 +0000

    Update build

--- src/functions/clear.js
+++
@@ -1,7 +0,0 @@
-function clear(){
-  const content = document.querySelector(".main");
-  while(content.firstElementChild){
-    content.firstElementChild.remove()
-  }
-}
-export {clear}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import { renderDefault } from "./functions/renderDefault"
-(function runBuild(){
-  renderDefault()
-
-}())
